cfbf91eccc24502570b23b6b549b857788031311
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2020-10-15 13:00:31 +0200

    Project Files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <h2>Have a look!</h2>
+    </div>
+  )
 }
+
+export default HomePage  
