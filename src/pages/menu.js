0dff738161a1613110fdbefb64ee81ae06e5d29e
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2020-10-16 13:06:02 +0200

    Generate pages dynamically

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
-import {graphql, useStaticQuery } from "gatsby"
+import {Link, graphql, useStaticQuery } from "gatsby"
 
 const MenuPage = () => {
     const data = useStaticQuery(graphql`
@@ -11,6 +11,9 @@ const MenuPage = () => {
                         frontmatter{
                             title
                             date
+                        }
+                        fields{
+                            slug
                         }
                     }
                 }
@@ -26,8 +29,10 @@ const MenuPage = () => {
                     {data.allMarkdownRemark.edges.map((edge) => {
                         return (
                             <li>
-                                <h2>{edge.node.frontmatter.title}</h2>
-                                <p>{edge.node.frontmatter.date}</p>
+                                <Link to={`/menu/${edge.node.fields.slug}`}>
+                                    <h2>{edge.node.frontmatter.title}</h2>
+                                    <p>{edge.node.frontmatter.date}</p>
+                                </Link>
                             </li>
                         )
                     })}
---
+++ src/templates/menu.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Menu = () => {
+    return (
+        <Layout>
+            This is the menu template
+        </Layout>
+    )
+}
+
+export default Menu
